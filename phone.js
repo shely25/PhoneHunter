3088e6dd5e70aa2babf892154523cf4700540700
shely25 <shely.ece.25@gmail.com>
2022-03-01 01:33:36 +0600

    Phones shown part added

--- phone.js
+++ phone.js
@@ -4,6 +4,33 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayResult(data.data))
+    searchfield.value = "";
+}
 
+const displayResult = phones => {
+    //console.log(phones);
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = "";
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <h5 class="card-title"> Brand : ${phone.brand}</h5>
+                <button onClick="SeeDetails()" class="btn btn-outline-secondary bg-dark text-white" type="button"
+                id="button-addon2">Detail</button>
+            </div>
+        </div>`;
+        searchResult.appendChild(div)
+
+    })
+}
+
+
+const SeeDetails = () => {
+    console.log("clicked")
 }
