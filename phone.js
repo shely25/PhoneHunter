0c06dfd3996276578dc0928abcbc49138c297385
shely25 <shely.ece.25@gmail.com>
2022-03-01 00:48:25 +0600

    fetch added -1

--- phone.js
+++ phone.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const searchPhone = () => {
+    const searchfield = document.getElementById('searchfield');
+    const searchValue = searchfield.value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+
+}
